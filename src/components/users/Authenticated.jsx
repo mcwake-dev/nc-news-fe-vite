27c4d7ad94aba343dbf081398a70654d5977a8e9
Matthew C Wake <mcwake-dev@outlook.com>
2022-07-12 21:57:02 +0100

    Switched to atoms from React Context for user management

--- src/components/users/Authenticated.jsx
+++ src/components/users/Authenticated.jsx
@@ -1,8 +1,14 @@
+import { useAtom } from "jotai";
+
+import { userAtom } from "../../atoms/users.atom";
+
 export const AUTHENTICATED_ONLY = true;
 export const UNAUTHENTICATED_ONLY = false;
 
 const Authenticated = (WrappedComponent, needsAuthentication) => {
   function AuthenticatedHOC(props) {
+    const [user, setUser] = useAtom(userAtom);
+
     if ((user && needsAuthentication) || (!user && !needsAuthentication)) {
       return <WrappedComponent {...props} setUser={setUser} user={user} />;
     } else {
