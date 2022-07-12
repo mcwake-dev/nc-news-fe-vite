51fd28bc759685c8084526d768a581dba0ca279a
Matthew C Wake <mcwake-dev@outlook.com>
2022-07-12 21:54:21 +0100

    Added atoms for filtering and retrieving filtered list of articles

--- src/atoms/articles.atom.js
+++ src/atoms/articles.atom.js
@@ -8,3 +8,28 @@ const ARTICLES_API = `${API}/articles`;
 export const highestVotedArticlesAtom = atom(async () =>
   axios.get(new URL(`${ARTICLES_API}/highest`))
 );
+
+export const filtersAtom = atom(["all", "all", "created_at", "desc"]);
+
+export const articlesAtom = atom(async (get) => {
+  const filterUrl = new URL(ARTICLES_API);
+  const [author, topic, sort, order] = get(filtersAtom);
+
+  if (author && author !== "all") {
+    filterUrl.searchParams.append("author", author);
+  }
+
+  if (topic && topic !== "all") {
+    filterUrl.searchParams.append("topic", topic);
+  }
+
+  if (sort) {
+    filterUrl.searchParams.append("sort_by", sort);
+  }
+
+  if (order) {
+    filterUrl.searchParams.append("order", order);
+  }
+
+  return axios.get(filterUrl);
+});
