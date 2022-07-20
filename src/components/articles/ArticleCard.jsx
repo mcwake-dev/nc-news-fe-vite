678a2a86683a30579734460cf7291cae25d17d02
Matthew C Wake <mcwake-dev@outlook.com>
2022-07-20 21:58:08 +0100

    Switched to using atoms for managing filters

--- src/components/articles/ArticleCard.jsx
+++ src/components/articles/ArticleCard.jsx
@@ -2,10 +2,13 @@ import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import * as dayjs from "dayjs";
 import { Link } from "react-router-dom";
+import { useAtom } from "jotai";
 
-import SortAndFilterLink from "../SortAndFilterLink";
+import { filtersAtom } from "../../atoms/articles.atom";
 import VoteControls from "../forms/VoteControls";
 import DeleteArticle from "./DeleteArticle";
+import TopicButton from "../TopicButton";
+import ArticleButton from "../ArticleButton";
 
 const ArticleCard = ({
   article: {
@@ -20,84 +23,86 @@ const ArticleCard = ({
   },
   children,
 }) => {
-  return (
-    <article>
-      <header>
-        {children ? (
-          <nav className="breadcrumb">
-            <ul>
-              <li>
-                <Link
-                  className="tag mx-2 is-size-5 py-0"
-                  to={"/author/all/topic/all/sort-by/created_at/order/desc"}
-                >
-                  Articles
-                </Link>
-              </li>
-              <li>
-                <SortAndFilterLink
-                  param={topic}
-                  title={
-                    topic ? topic[0].toUpperCase() + topic.substring(1) : ""
-                  }
-                  linkType={"topic"}
-                />
-              </li>
-              <li>
-                <Link
-                  className="tag mx-2 is-size-5 py-0"
-                  to={`/articles/${article_id}`}
-                >
-                  {title}
-                </Link>
-              </li>
-            </ul>
-          </nav>
-        ) : (
-          <></>
-        )}
-        <h1 className="title">{title}</h1>
-        <h2 className="subtitle">
-          Posted by{" "}
-          <SortAndFilterLink
-            param={author}
-            title={author}
-            linkType={"author"}
-          />{" "}
-          on{" "}
-          {`${dayjs(created_at).format("ddd D MMM YYYY")} at ${dayjs(
-            created_at
-          ).format("HH:mm")}`}
-        </h2>
-      </header>
-      <div>
-        {body ? (
-          <VoteControls
-            voteType={"article"}
-            item_id={article_id}
-            votes={votes}
-          />
-        ) : (
-          <>
-            <p>{votes} votes</p>
-            <p>{comment_count} comments</p>
-          </>
-        )}
-      </div>
+  const [filters, setFilters] = useAtom(filtersAtom);
 
-      <main>
-        <ReactMarkdown children={body} remarkPlugins={[remarkGfm]} />
-        <DeleteArticle article_id={article_id} author={author} />
-      </main>
-      {body ? (
-        <>
-          <h2 className="subtitle mb-0">Comments</h2>
-          {children}
-        </>
+  return (
+    <>
+      {children ? (
+        <nav className="breadcrumb">
+          <ul>
+            <li>
+              <Link
+                to={`/author/${filters.author}/topic/${filters.topic}/sort-by/${filters.sort_by}/order/${filters.order}`}
+              >
+                Articles
+              </Link>
+            </li>
+            <li>
+              <TopicButton topic={topic} />
+            </li>
+            <li>
+              <ArticleButton articleId={article_id} articleTitle={title} />
+            </li>
+          </ul>
+        </nav>
       ) : (
         <></>
       )}
-    </article>
+
+      <article className="article">
+        <header>
+          <div className="user-header">
+            <div className="colour-bar"></div>
+            <h3>
+              <i className="fa fa-user"></i>{" "}
+              {/* <SortAndFilterLink
+                param={author}
+                title={author}
+                linkType={"author"}
+              /> */}
+            </h3>
+            <h3>
+              <i className="fa fa-calendar"></i>{" "}
+              {`${dayjs(created_at).format("DD MMM YYYY")}`}
+            </h3>
+            <h3>
+              <i className="fa fa-clock-o"></i>{" "}
+              {`${dayjs(created_at).format("HH:mm")}`}
+            </h3>
+          </div>
+          <h2 className="title">
+            <ArticleButton articleId={article_id} articleTitle={title} />
+          </h2>
+        </header>
+        <footer className="article-footer">
+          {body ? (
+            <VoteControls
+              voteType={"article"}
+              item_id={article_id}
+              votes={votes}
+            />
+          ) : (
+            <>
+              <p>{votes} votes</p>
+              <p>{comment_count} comments</p>
+            </>
+          )}
+        </footer>
+
+        <main>
+          <ReactMarkdown children={body} remarkPlugins={[remarkGfm]} />
+          <DeleteArticle article_id={article_id} author={author} />
+        </main>
+        {body ? (
+          <>
+            <h2 className="subtitle mb-0">Comments</h2>
+            {children}
+          </>
+        ) : (
+          <></>
+        )}
+      </article>
+    </>
   );
 };
 
